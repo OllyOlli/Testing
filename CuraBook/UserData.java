94b2bbe75073b0e618da19718399d201935864be
Olly <124927810+OllyOlli@users.noreply.github.com>
2024-08-28 23:10:31 +0200

    Create UserData.java

---
+++ CuraBook/UserData.java
@@ -0,0 +1,42 @@
+package com.curabook.models;
+
+// Model class for user data
+public class UserData {
+    private String username;
+    private String password;
+    private String url;
+    private boolean hasError;
+
+    // Getters and setters for user data fields
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public boolean isHasError() {
+        return hasError;
+    }
+
+    public void setHasError(boolean hasError) {
+        this.hasError = hasError;
+    }
+}
